b926e497a70ee0203682a984b11a270b0d9a1460
pouyehbnj <pouyehbnj@gmail.com>
2022-12-18 10:06:02 +0100

    adding client loop

--- analyzer.py
+++ analyzer.py
@@ -1,12 +1,12 @@
 import paho.mqtt.client as mqtt
-
+import time
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("")
+    client.subscribe("energy/prediction")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -23,5 +23,7 @@ client.connect("localhost", 1883, 60)
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
+client.loop_start()
+time.sleep(4)
+client.loop_stop()
 # test
